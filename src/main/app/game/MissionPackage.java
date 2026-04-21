bca7b0083b19662a4d371277923265741a470650
Owen Treichler <treichlerop@gmail.com>
2026-04-20 21:39:52 -0700

    Working on MissionPackage.java

---
+++ src/main/app/game/MissionPackage.java
@@ -0,0 +1,27 @@
+package app.game;
+
+/**
+ * - A mission package is the Asciia equivalent of a campaign.
+ * - It is played by ground control (the players in real life) talking to a
+ * relay space station (the person acting as DM).
+ * - Ground control must guide the rovers (players in the game) to carry out a
+ * mission.
+ * - A mission is broken into objectives.
+ * - Objectives are small tasks that must be completed in order to complete the
+ * overall mission and return home.
+ * - A mission package contains a map, rovers, NPCs, items, and various other
+ * elements that can be interacted with.
+ * - A mission begins at a drop point (starting location on the map), and the
+ * rovers will go to different places to collect materials and complete
+ * objectives.
+ * - Some objectives will be harder than others, so it is important that ground
+ * control strategizes and plans out the best way to complete the mission.
+ * - Objectives will become easier as more materials are collected, so it is
+ * important to prioritize objectives and collect materials as early as
+ * possible.
+ * - Once all objectives are completed, the rovers must return to the drop point
+ * to repair the ship and return home.
+ */
+public class MissionPackage {
+
+}
